b0581e28ff45a631d20936c112481145ed542033
Redpador <resumidorpandora@gmail.com>
2024-12-12 01:56:05 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+document.getElementById('convertForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+    const message = document.getElementById('message');
+    message.textContent = 'Esta funcionalidad estará disponible pronto.';
+});
